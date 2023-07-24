fe6a4614316b799e5401ffaabca9034b73f0122b
07Roshi <roshithshetty7@gmail.com>
2023-07-24 17:12:32 +0530

    update

---
+++ exam/src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary text-primary text-center py-0 mt-1 fixed-bottom">
+      <div className="container py-0 mt-1 text-dark">
+        <p>© Copyright 2021-2025</p>
+        <p>All rights reserved & Powered by RS Lunch Home</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
